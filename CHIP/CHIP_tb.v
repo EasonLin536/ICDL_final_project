de21d15360b98c4720c7ec9e2a1486a455e3f9c2
EasonLin536 <b06901153@ntu.edu.tw>
2020-05-20 16:45:02 +0800

    try loading pixel

--- CHIP/CHIP_tb.v
+++ CHIP/CHIP_tb.v
@@ -10,11 +10,12 @@ module tb();
 	// parameter OUT_LENGTH    = 100;
 	// parameter DATA_LENGTH   = OUT_LENGTH + 2;  // padding
 
-	reg        clk, reset, mode;
+	reg        clk, reset, mode, load_end;
 	reg  [4:0] pixel_in0;
 	reg  [4:0] pixel_in1;
 	reg  [4:0] pixel_in2;
 	wire [4:0] pixel_out;
+	wire [4:0] edge_out;
     // wire       readable;
 	
  //    reg  [4:0] out_mem    [0:OUT_LENGTH-1];
@@ -27,7 +28,7 @@ module tb();
 	// integer    i, j, out_f, err, pattern_num;
 	// reg        over;
 
-	Main_Ctrl_Unit mcu ( clk, reset, mode, pixel_in0, pixel_in1, pixel_in2, pixel_out );
+	CHIP chip ( clk, reset, mode, pixel_in0, pixel_in1, pixel_in2, edge_out, pixel_out, load_end );
 	
 	// initial	$readmemb (`PIXEL1,  pixel1_mem);
 	// initial	$readmemb (`PIXEL2,  pixel2_mem);
@@ -40,11 +41,12 @@ module tb();
     end
 
     initial begin
-     	clk         = 1'b1;
+     	clk         = 1'b0;
      	reset       = 1'b0;
      	pixel_in0   = 5'd0;
 		pixel_in1   = 5'd0;
 		pixel_in2   = 5'd0;
+		load_end = 1'b0;
      	// enable      = 1'b0;
      	// stop        = 1'b0;  
    		// over        = 1'b0;
@@ -59,7 +61,7 @@ module tb();
 	always begin #(`CYCLE/2) clk = ~clk; end
 
 	initial begin
-		#(`CYCLE)
+		#(`CYCLE*2)
 		pixel_in0 = 5'd1;
 		pixel_in1 = 5'd1;
 		pixel_in2 = 5'd1;
@@ -68,9 +70,11 @@ module tb();
 		pixel_in1 = 5'd2;
 		pixel_in2 = 5'd2;
 		#(`CYCLE)
+		load_end = 1'b1;
 		pixel_in0 = 5'd3;
 		pixel_in1 = 5'd3;
 		pixel_in2 = 5'd3;
+		#(`CYCLE)
 		$finish;
 	end
 
