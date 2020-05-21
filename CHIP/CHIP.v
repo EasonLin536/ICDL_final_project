b7727674cacc789395e2afb31717f282174a1706
EasonLin536 <b06901153@ntu.edu.tw>
2020-05-21 20:04:57 +0800

    WRITE_BACK done

--- CHIP/CHIP.v
+++ CHIP/CHIP.v
@@ -1,7 +1,7 @@
 `define IMG_DIM    20
 `define BIT_LENGTH 5
 `define TOTAL_REG  `IMG_DIM * `IMG_DIM
-`define TMP_REG    (`IMG_DIM - 2) * (`IMG_DIM - 2)
+`define ANG_REG    (`IMG_DIM - 2) * (`IMG_DIM - 2)
 
 module CHIP ( clk, reset, pixel_in0, pixel_in1, pixel_in2, pixel_in3, pixel_in4, edge_out, load_end, readable );
 	input                      clk, reset, load_end;
@@ -69,8 +69,8 @@ module CHIP ( clk, reset, pixel_in0, pixel_in1, pixel_in2, pixel_in3, pixel_in4,
 
 // =============== Register File =============== //
     reg [`BIT_LENGTH - 1:0] reg_img   [0:`TOTAL_REG - 1];
-    reg [`BIT_LENGTH - 1:0] reg_tmp   [0:`TMP_REG - 1];
-    reg               [1:0] reg_angle [0:`TMP_REG - 1];
+    reg [`BIT_LENGTH - 1:0] reg_tmp   [0:`TOTAL_REG - 1];
+    reg               [1:0] reg_angle [0:`ANG_REG - 1];
 
 // =============== Combinational =============== //
 	/* FSM */
@@ -113,6 +113,7 @@ module CHIP ( clk, reset, pixel_in0, pixel_in1, pixel_in2, pixel_in3, pixel_in4,
 	end
 
 	// ind initializeation
+	
 
 	/* PREPARE */
 	// assign ind_col_end
@@ -126,22 +127,78 @@ module CHIP ( clk, reset, pixel_in0, pixel_in1, pixel_in2, pixel_in3, pixel_in4,
 
 	/* WRITE_BACK */
 	// write tmp to img registers
+	always @(*) begin
+		if (state == WRITE_BACK) begin
+			for (i=0;i<`TOTAL_REG;i=i+1) reg_img[i] = reg_tmp[i];
+			if (operation == GAU_FIL) begin
+				// 4 corners
+				reg_img[0:1]     = reg_img[42];
+				reg_img[20:21]   = reg_img[42];
+				reg_img[18:19]   = reg_img[57];
+				reg_img[38:39]   = reg_img[57];
+				reg_img[360:361] = reg_img[342];
+				reg_img[380:381] = reg_img[342];
+				reg_img[378:379] = reg_img[357];
+				reg_img[398:399] = reg_img[357];
+				// horizontal sides
+				reg_img[2:17]    = reg_img[42:57];
+				reg_img[22:37]   = reg_img[42:57];
+				reg_img[362:377] = reg_img[342:357];
+				reg_img[382:397] = reg_img[342:357];
+				// vertical sides
+				for (i=40;i<360;i=i+20) begin
+					reg_img[i]    = reg_img[i+2];
+					reg_img[i+1]  = reg_img[i+2];
+					reg_img[i+18] = reg_img[i+17];
+					reg_img[i+19] = reg_img[i+17];
+				end
+			end
+			else begin
+				// 4 corners
+				reg_img[0]   = reg_img[21];
+				reg_img[19]  = reg_img[38];
+				reg_img[380] = reg_img[361];
+				reg_img[399] = reg_img[378];
+				// horizontal sides
+				reg_img[1:18]    = reg_img[21:38];
+				reg_img[381:398] = reg_img[361:378];
+				// vertical sides
+				for (i=20;i<380;i=i+20) begin
+					reg_img[i]    = reg_img[i+1];
+					reg_img[i+19] = reg_img[i+18];
+				end
+			end
+		end
+		else for (i=0;i<`TOTAL_REG;i=i+1) reg_img[i] = reg_img[i];
+	end
 
 // ================ Sequential ================= //
 	always @(posedge clk or posedge reset) begin
 		if (reset) begin
+			state      <= LOAD_REG;
 			load_index <= 9'd0;
-			state     <= LOAD_REG;
-			operation <= MED_FIL;
+			operation  <= MED_FIL;
+			ind_0_r    <= 9'd0;
+			ind_1_r    <= 9'd0;
+			ind_2_r    <= 9'd0;
+			ind_3_r    <= 9'd0;
+			ind_4_r    <= 9'd0;
+			edge_out_r <= 1'b0;
 		end
 		else begin
+			state      <= state_next;
 			load_index <= load_index + 5;
-			state     <= state_next;
-			operation <= operation_next;
+			operation  <= operation_next;
+			ind_0_r    <= ind_0_w;
+			ind_1_r    <= ind_1_w;
+			ind_2_r    <= ind_2_w;
+			ind_3_r    <= ind_3_w;
+			ind_4_r    <= ind_4_w;
+			edge_out_r <= edge_out_w;
 		end
 	end
 
-	// load pixels into img register file
+	// LOAD_REG
 	always @(posedge clk or posedge reset) begin
 		if (reset) begin
 			for (i=0;i<`TOTAL_REG;i=i+1) reg_img[i] <= 5'd0;
@@ -160,4 +217,5 @@ module CHIP ( clk, reset, pixel_in0, pixel_in1, pixel_in2, pixel_in3, pixel_in4,
 			end
 		end
 	end
+
 endmodule
