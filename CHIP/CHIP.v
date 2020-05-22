4bc94c7a85775b16e5e2c77f4184c919e914500d
EasonLin536 <b06901153@ntu.edu.tw>
2020-05-22 11:00:45 +0800

    PREPARE done

--- CHIP/CHIP.v
+++ CHIP/CHIP.v
@@ -135,7 +135,7 @@ module CHIP ( clk, reset, pixel_in0, pixel_in1, pixel_in2, pixel_in3, pixel_in4,
 		end
 	end
 
-	// ind initializeation
+	// ind initialization
 	always @(*) begin
 		if (state == SET_OP) begin
 			ind_0_w = 9'd0;
