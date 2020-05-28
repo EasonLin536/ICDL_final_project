18d17230f6b17f5ae122d1a8b17839580aa5b822
EasonLin536 <b06901153@ntu.edu.tw>
2020-05-28 10:07:56 +0800

    multi-tile load

--- CHIP/CHIP_tb.v
+++ CHIP/CHIP_tb.v
@@ -1,15 +1,16 @@
 `timescale 1ns/10ps
 `define CYCLE 10
-`define PIXEL0 "./Testbench/pattern/input_pixel/pixel_in0.dat"  
-`define PIXEL1 "./Testbench/pattern/input_pixel/pixel_in1.dat"  
-`define PIXEL2 "./Testbench/pattern/input_pixel/pixel_in2.dat"
-`define PIXEL3 "./Testbench/pattern/input_pixel/pixel_in3.dat"
-`define PIXEL4 "./Testbench/pattern/input_pixel/pixel_in4.dat"
-`define EXPECT "./Testbench/pattern/Hysteresis/out_golden.dat"
+// modify pattern path
+`define PIXEL0 "./pattern/pixel_in0.dat"  
+`define PIXEL1 "./pattern/pixel_in1.dat"  
+`define PIXEL2 "./pattern/pixel_in2.dat"
+`define PIXEL3 "./pattern/pixel_in3.dat"
+`define PIXEL4 "./pattern/pixel_in4.dat"
+`define EXPECT "./pattern/out_golden.dat"
 
 module tb();
 
-	parameter INPUT_TILE  = 12
+	parameter INPUT_TILE  = 2; // modify for # of input tile
 	parameter DATA_LENGTH = 80 * INPUT_TILE;
 	parameter OUT_LENGTH  = 18*18 * INPUT_TILE;
 
@@ -20,8 +21,6 @@ module tb();
 	reg  [4:0] pixel_in3;
 	reg  [4:0] pixel_in4;
 	wire       edge_out;
-	// wire [4:0] debug_pixel;
-	// wire [1:0] debug_angle;
     
 	reg  [4:0] pixel_mem  [0:OUT_LENGTH-1];
 	reg  [4:0] angle_mem  [0:OUT_LENGTH-1];
@@ -31,8 +30,8 @@ module tb();
 	reg  [4:0] pixel3_mem [0:DATA_LENGTH-1];
 	reg  [4:0] pixel4_mem [0:DATA_LENGTH-1];
 	reg  [4:0] pixel_temp;
-	// reg  [4:0] angle_temp;
 
+	reg        in_pause;
 	reg        stop;
 	integer    i, j, k, out_f, err, pattern_num;
 	reg        over;
@@ -54,16 +53,16 @@ module tb();
     initial begin
      	clk         = 1'b0;
      	reset       = 1'b0;
-		in_pause    = 1'b0; // finish 1 tile of input, wait for output
-     	stop        = 1'b0;
+     	stop        = 1'b0;  
    		over        = 1'b0;
    		pattern_num = 0;
    		err         = 0;
-   		i           = 0; // input index
-   		j           = 0; // output index
+   		i           = 0;
+   		j           = 0;
 		load_end    = 1'b0;
-   		// #2.5 reset  = 1'b1;
-    	// #5 reset    = 1'b0;
+		in_pause    = 1'b0;
+   		#2.5 reset  = 1'b1;
+    	#5 reset    = 1'b0;
 	end
 
 	always begin #(`CYCLE/2) clk = ~clk; end
@@ -73,15 +72,6 @@ module tb();
 		if (i < DATA_LENGTH) begin
 			// if we should load input pixels again
 			if (!in_pause) begin
-				if (i % 80 == 0) begin
-					#2.5 reset = 1'b1;
-					#5   reset = 1'b0;
-				end
-
-				if ((i + 1) % 80 == 0) begin
-					in_pause = 1'b1;
-					load_end = 1'b1;
-				end
 
 				pixel_in0 = pixel0_mem[i];
 				pixel_in1 = pixel1_mem[i];
@@ -90,17 +80,27 @@ module tb();
 				pixel_in4 = pixel4_mem[i];
 				
 				i = i + 1;
+
+				if (i % 80 == 0 && i != 0) begin
+					#2.5 reset = 1'b1;
+					#5   reset = 1'b0;
+				end
+
+				if ((i + 1) % 80 == 0) begin
+					in_pause = 1'b1;
+					load_end = 1'b1;
+				end	
 			end
 		end
-		else begin
-			stop = 1;
-		end
 	end
-
+	
 	always @(negedge clk) begin
 		if (j < OUT_LENGTH && readable) begin
 
-			if ((j + 1) % 324 == 0) in_pause = 1'b0;
+			if ((j + 1) % 324 == 0) begin
+				in_pause = 1'b0;
+				load_end = 1'b0;
+	     	end
 
 			pixel_temp = pixel_mem[j];
 			j = j + 1;
@@ -116,19 +116,50 @@ module tb();
 	    end
 	end
 
+	// initial begin
+	// 	for (i=0;i<DATA_LENGTH;i=i+1) begin
+	// 		if (i==79) load_end = 1'b1;
+			
+	// 		#(`CYCLE);
+			
+	// 		pixel_in0 = pixel0_mem[i];
+	// 		pixel_in1 = pixel1_mem[i];
+	// 		pixel_in2 = pixel2_mem[i];
+	// 		pixel_in3 = pixel3_mem[i];
+	// 		pixel_in4 = pixel4_mem[i];
+	// 	end
+	// 	stop = 1;
+	// end
+
+	// always @(negedge clk) begin
+	// 	if (j < OUT_LENGTH && readable) begin
+	// 		pixel_temp = pixel_mem[j];
+	// 		j = j + 1;
+			
+	// 	    // modilfy for different module debugging : only when Full(i.e. Hysteresis)
+	// 	    if (edge_out !== pixel_temp) begin
+	// 	        $display("ERROR at %d:output %d !=expect %d ", pattern_num, edge_out, pixel_temp);
+	// 		    $fdisplay(out_f,"ERROR at %d:output %d !=expect %d ", pattern_num, edge_out, pixel_temp);
+	// 	        err = err + 1 ;
+	// 	    end
+	// 	    // comment out if not Full(i.e. Hysteresis)
+
+	// 	    pattern_num = pattern_num + 1; 
+	// 		if (pattern_num === OUT_LENGTH) over = 1'b1;
+	//     end
+	// end
+
 	initial begin
 	    @(posedge over)      
-	    if (stop) begin
-	       	$display("---------------------------------------------\n");
-	       	if (err == 0)  begin
-	          	$display("All data have been generated successfully!\n");
-	          	$display("-------------------PASS-------------------\n");
-	       	end
-	       	else begin
-	          	$display("There are %d errors!\n", err);
-			end
-	         $display("---------------------------------------------\n");
-	    end
+       	$display("---------------------------------------------\n");
+       	if (err == 0)  begin
+          	$display("All data have been generated successfully!\n");
+          	$display("-------------------PASS-------------------\n");
+       	end
+       	else begin
+          	$display("There are %d errors!\n", err);
+		end
+        $display("---------------------------------------------\n");
 	    
 	    #10 $finish;
 	end
